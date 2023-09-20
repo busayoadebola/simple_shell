770e1e2c4ff5290ab2c7e91d10bc84d98845ed44
Yusuff Omotoyosi <azeezyusuff2017@gmail.com>
2023-09-19 21:57:19 -0700

    betty

---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "Hello" to the console.
+ *
+ * Return 0
+ */
+int main(void)
+{
+    printf("Hello\n");
+    return (0);
+}
